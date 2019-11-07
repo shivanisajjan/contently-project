bcddc8bc49d4e7c467287c0c60b87381145b75fd
shivanisajjan <shivanisajjan269@gmail.com>
2019-11-07 18:36:13 +0530

    modified

--- profile-service/src/main/java/com/stackroute/profileservice/controller/ProfileController.java
+++ profile-service/src/main/java/com/stackroute/profileservice/controller/ProfileController.java
@@ -4,10 +4,6 @@ import com.stackroute.profileservice.model.Profile;
 import com.stackroute.profileservice.service.ProfileService;
 import com.stackroute.profileservice.service.ProfileServiceImpl;
 import com.stackroute.profileservice.service.RabbitMQSender;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiResponse;
-import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,7 +15,6 @@ import java.util.List;
 @RestController
 @RequestMapping(value = "/api/v1/profile")
 @CrossOrigin(origins = "*", allowedHeaders = "*")
-@Api(value="profile", description="profileservice")
 public class ProfileController {
     private ProfileService profileService;
     private ResponseEntity responseEntity;
@@ -32,7 +27,7 @@ public class ProfileController {
         this.rabbitMQSender=rabbitMQSender;
     }
 
-    @ApiOperation(value = "saves new profile")
+
     @PostMapping
     public ResponseEntity<?> saveProfile(@RequestBody Profile profile)  {
         profile.setId(profileService.getNextSequence("customSequences"));
@@ -41,19 +36,11 @@ public class ProfileController {
         return responseEntity;
     }
 
-    @ApiOperation(value = "get all profiles")
     @GetMapping
     public List<Profile> getAllProfile() {
         return (this.profileService.getallProfile());
     }
 
-
-
-    @ApiOperation(value = "updates the existing track with the new track")
-    @ApiResponses(
-            value = { @ApiResponse(code = 401,message = "unauthorized"),
-                    @ApiResponse(code = 201,message = "returning the whole track")}
-    )
     @PutMapping
     public ResponseEntity<?> updateMovie(@RequestBody Profile profile) {
         profileService.update(profile);
@@ -61,7 +48,7 @@ public class ProfileController {
         return responseEntity;
     }
 
-    @ApiOperation(value = "deletes profile")
+
     @DeleteMapping
     public ResponseEntity<?> deleteMovie(@RequestBody int id){
         profileService.deleteProfile(id);
